ddee2c3dd1f89b13958ec7a422a1b6a81eeda20f
GPoornimaGhodke <59560687+GPoornimaGhodke@users.noreply.github.com>
2020-02-03 14:19:04 +0530

    created

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+int main()
+{
+    int a,b, subb=0;
+    printf("enter value of a\n");
+    scanf("%d",&a);
+    printf("enter value of b\n");
+    scanf("%d",&b);
+    subb = a-b ;
+    printf("sub of a and b is sub= %d\n",subb);
+    return 0;
+}
